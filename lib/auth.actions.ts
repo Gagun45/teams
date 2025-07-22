fde59ae6d01b552004d5e9d4c1c95bd841b2d1d9
Gagun45 <selyanchyn45@gmail.com>
2025-07-22 19:29:31 +0300

    pre deploy2

--- lib/auth.actions.ts
+++ lib/auth.actions.ts
@@ -30,32 +30,3 @@ export const register = async (values: RegisterFormType) => {
     return { error: "Something went wrong" };
   }
 };
-
-// export const login = async (values: LoginFormType) => {
-//   const validatedFields = loginSchema.safeParse(values);
-//   if (!validatedFields.success) return { error: "Invalid fields" };
-//   const { email, password } = validatedFields.data;
-
-//   const existingUser = await getUserByEmail(email);
-
-//   if (!existingUser) return { error: "Email not found" };
-//   if (!existingUser.password) return { error: "Password not set yet" };
-
-//   const passwordMatch = await compare(password, existingUser.password);
-
-//   if (!passwordMatch) return { error: "Credentials invalid" };
-
-//   try {
-//     await signIn("credentials", { email, password });
-//     return { success: "Logged in" };
-//   } catch (err) {
-//     if (err instanceof AuthError) {
-//       return { error: "Something went wrong" };
-//     }
-//     throw err;
-//   }
-// };
-
-// export const logout = async () => {
-//   await signOut();
-// };
